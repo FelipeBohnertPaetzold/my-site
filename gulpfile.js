363f8e2f74b3e763c0be586193fc30f7e3dfe619
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2023-07-15 22:41:39 -0300

    test build

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,10 @@ const sass = require('gulp-sass')(require('sass'));
 
 
 function buildStyles() {
-  return gulp.src('./sass/app.scss')
+  return gulp.src('sass/app.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('public/css'));
 }
-
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
 
 function defaultTask(cb) {
   buildStyles();
