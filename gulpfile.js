ccc074435c6f98be71ee5494b5109601e82144f8
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2023-07-15 22:56:51 -0300

    configure deploy

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return gulp.src('sass/app.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('public/css'));
 }
 
 function defaultTask(cb) {
